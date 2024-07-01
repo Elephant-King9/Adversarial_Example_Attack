f53c8c2f881e592d84a9bf35778447680d6af4b6
Elephant-King9 <s976901478@163.com>
2024-07-01 15:38:35 +0800

    更新frost攻击图像

--- attacks/attack_frost.py
+++ attacks/attack_frost.py
@@ -13,6 +13,8 @@ class attack_frost:
         self.model = model
 
     def attack(self, image, epsilon, label):
+        
+        image = image * 255
         # 将tensor数据类型的图片转化为numpy
         image = image.cpu().numpy()
         logger.debug(f'image shape before:{image.shape}')  # image shape:(1, 3, 480, 480)
@@ -65,7 +67,7 @@ class attack_frost:
             x_start, y_start = 0, 0
             frost = frost[x_start:x_start + h, y_start:y_start + w][..., [2, 1, 0]]
 
-        perturbed_image = np.clip(c[0] * np.array(image) + c[1] * frost, 0, 1)
+        perturbed_image = np.clip(c[0] * np.array(image) + c[1] * frost, 0, 255) / 255
         perturbed_image = perturbed_image.transpose((2, 0, 1))
         perturbed_image = torch.from_numpy(perturbed_image).float().to(self.config.device)
         perturbed_image = perturbed_image.unsqueeze(0)
